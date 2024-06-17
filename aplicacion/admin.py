d19eee155004f74794911a8782507a27b7e331b9
samueleliazar <samu.reyes@duocuc.cl>
2024-06-17 19:45:49 -0400

    persona

--- aplicacion/admin.py
+++ aplicacion/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Persona
 
 # Register your models here.
+
+class AdmPersona(admin.ModelAdmin):
+    list_display=['nombre', 'apellido', 'fecha_crea', 'direccion', 'telefono', 'correo']
+    list_filter=['apellido']
+    
+admin.site.register(Persona,AdmPersona)
--- aplicacion/models.py
+++ aplicacion/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
 # Create your models here.
+
+
+class Persona(models.Model):
+    fecha_crea=models.DateField(verbose_name='Fecha de creacion')
+    nombre=models.CharField(max_length=50,null=False)
+    apellido=models.CharField(max_length=50,null=False)
+    direccion=models.CharField(max_length=100,null=False)
+    telefono=models.IntegerField()
+    correo=models.EmailField(error_messages='Introduce bien el correo')
