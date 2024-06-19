09c69bbda128181350fba353024277142b380f78
samueleliazar <samu.reyes@duocuc.cl>
2024-06-18 23:28:29 -0400

    productos

--- aplicacion/admin.py
+++ aplicacion/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Persona
+from .models import Persona, Producto
 
 # Register your models here.
 
@@ -7,4 +7,9 @@ class AdmPersona(admin.ModelAdmin):
     list_display=['nombre', 'apellido', 'fecha_crea', 'direccion', 'telefono', 'correo']
     list_filter=['apellido']
     
+    
+class AdmProducto(admin.ModelAdmin):
+    list_display=['nombre', 'descripcion']
+
 admin.site.register(Persona,AdmPersona)
+admin.site.register(Producto,AdmProducto)
--- aplicacion/models.py
+++ aplicacion/models.py
@@ -9,3 +9,12 @@ class Persona(models.Model):
     direccion=models.CharField(max_length=100,null=False)
     telefono=models.IntegerField()
     correo=models.EmailField(error_messages='Introduce bien el correo')
+
+
+class Producto (models.Model):
+    nombre=models.CharField(max_length=100,null=False)
+    descripcion=models.CharField(max_length=200,null=False)
+    precio=models.IntegerField(null=False)
+    imagen=models.ImageField(upload_to='nuevosproductos',null=False)
+    
+    
