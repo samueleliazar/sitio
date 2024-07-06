42dc0acbe199d53a77d66c5e05eec3861522863d
samueleliazar <samu.reyes@duocuc.cl>
2024-07-06 19:35:34 -0400

    s

--- aplicacion/static/js/api.js
+++ aplicacion/static/js/api.js
@@ -1,12 +1,12 @@
-// Función para obtener una imagen de gato de la API
+
 function getCatImage() {
     fetch('https://api.thecatapi.com/v1/images/search')
       .then(response => response.json())
       .then(data => {
-        // Aquí data es un arreglo con información de la imagen, pero solo necesitas la URL
+        
         const imageUrl = data[0].url;
         
-        // Ahora puedes usar la URL para mostrar la imagen en tu página
+        
         const catImageElement = document.getElementById('cat-image');
         catImageElement.src = imageUrl;
       })
@@ -15,5 +15,5 @@ function getCatImage() {
       });
   }
   
-  // Llama a la función para obtener una imagen de gato cuando la página se cargue
+  
   document.addEventListener('DOMContentLoaded', getCatImage);
